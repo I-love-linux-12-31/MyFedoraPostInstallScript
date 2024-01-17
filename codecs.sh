b39996ba68d0eca7bd071d64dab91360a4a845d2
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2024-01-17 18:14:25 +0300

    Added codecs install script

--- codecs.sh
+++ codecs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-plugin-libav --exclude=gstreamer1-plugins-bad-free-devel
-dnf install -y lame\* --exclude=lame-devel
-dnf group upgrade -y --with-optional Multimedia
+dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-plugin-libav --exclude=gstreamer1-plugins-bad-free-devel --allowerasing --skip-broken
+dnf install -y lame\* --exclude=lame-devel --allowerasing --skip-broken
+dnf group upgrade -y --with-optional Multimedia --allowerasing --skip-broken
