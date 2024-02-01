bdf7cc5b8bec825d30ff297c6716db67737231bf
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2024-02-01 21:38:51 +0300

    Added some software

--- flatpack_software.sh
+++ flatpack_software.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y org.linux_hardware.hw-probe io.github.shiftey.Desktop io.github.seadve.Mousai dev.geopjr.Collision com.belmoussaoui.Obfuscate com.belmoussaoui.Decoder io.github.bytezz.IPLookup io.github.realmazharhussain.GdmSettings com.heroicgameslauncher.hgl im.bernard.Nostalgia io.github.Rirusha.Cassette
+flatpak install -y org.linux_hardware.hw-probe io.github.shiftey.Desktop io.github.seadve.Mousai dev.geopjr.Collision com.belmoussaoui.Obfuscate com.belmoussaoui.Decoder io.github.bytezz.IPLookup io.github.realmazharhussain.GdmSettings com.heroicgameslauncher.hgl im.bernard.Nostalgia io.github.Rirusha.Cassette io.github.arunsivaramanneo.GPUViewer de.haeckerfelix.Shortwave com.github.ADBeveridge.Raider ca.desrt.dconf-editor
--- useful_software.sh
+++ useful_software.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-dnf install -y krita chromium videomorph vlc python-vlc remmina-gnome-session remmina-devel remmina-plugins-*
+dnf install -y krita chromium videomorph vlc python-vlc remmina-gnome-session remmina-devel remmina-plugins-* neofetch blender
+dnf install -y steam komikku
+dnf install libvirt virt-manager
