669a64091d7774d7e9596b6835227156c426cf0e
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2024-02-01 22:59:36 +0300

    -

--- useful_software.sh
+++ useful_software.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dnf install -y krita chromium videomorph vlc python-vlc remmina-gnome-session remmina-devel remmina-plugins-* neofetch blender
 dnf install -y steam komikku
-dnf install libvirt virt-manager
+dnf install -y libvirt virt-manager
