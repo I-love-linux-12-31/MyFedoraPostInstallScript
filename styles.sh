926e4bb6ac7381ff394286e713ecdc0a109860e8
Yaroslav <94708058+I-love-linux-12-31@users.noreply.github.com>
2024-02-01 23:29:44 +0300

    Added flat-remix gnome-shell theme.

--- styles.sh
+++ styles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dnf install -y gnome-extensions-app gnome-tweaks
 dnf install -y gnome-shell-extension-user-theme gnome-shell-extension-gsconnect gnome-shell-extension-dash-to-dock gnome-shell-extension-apps-menu gnome-shell-extension-appindicator gnome-shell-extension-system-monitor-applet gnome-terminal-nautilus
-dnf install -y flat-remix-gtk4-theme flat-remix-icon-theme
+dnf install -y flat-remix-gtk4-theme flat-remix-icon-theme flat-remix-theme gnome-shell-theme-flat-remix
 cp ./res/.bashrc_root ~/.bashrc
