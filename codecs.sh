b0bcada3a2345e2d4a3ee018fece562b02faa74e
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2024-06-08 18:50:27 +0300

    Added ffmpeg and some other software

--- codecs.sh
+++ codecs.sh
@@ -2,3 +2,4 @@
 dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-plugin-libav --exclude=gstreamer1-plugins-bad-free-devel --allowerasing --skip-broken
 dnf install -y lame\* --exclude=lame-devel --allowerasing --skip-broken
 dnf group upgrade -y --with-optional Multimedia --allowerasing --skip-broken
+dnf install -y ffmpeg ffmpeg-libs
--- useful_software.sh
+++ useful_software.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+dnf install -y gedit
 dnf install -y krita chromium videomorph vlc python-vlc remmina-gnome-session remmina-devel remmina-plugins-* neofetch blender
 dnf install -y steam komikku
 dnf install -y libvirt virt-manager
+dnf install -y firefox google-chrome-stable
