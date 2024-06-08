5b4bbee7617ca54ac8d776542266f2cc4f5e05f0
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2024-06-08 18:31:52 +0300

    Added GNOME installation for Server edition of Fedora

--- base_system.sh
+++ base_system.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# For Fedora Server
+dnf groupinstall -y GNOME Desktop Environment
+
 dnf install -y openssh-server openssh mc htop python3 lm_sensors lm_sensors-libs java-latest-openjdk java-17-openjdk java-17-openjdk-headless java-17-openjdk-devel java-11-openjdk java-1.8.0-openjdk java-1.8.0-openjdk-openjfx java-1.8.0-openjdk-openjfx-devel
 dnf install flatpack -y
 dnf install -y NetworkManager-ssh-gnome NetworkManager-openvpn-gnome NetworkManager-vpnc-gnome NetworkManager-openconnect-gnome NetworkManager-l2tp-gnome
